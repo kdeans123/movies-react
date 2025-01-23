ee0bb348b5456d08492d1e03a091773eecc6258d
Kat <kat.m.deans@gmail.com>
2025-01-22 22:02:04 -0500

    Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Movies from "./pages/Movies";
 import Nav from "./components/Nav";
 import Landing from "./components/Landing";
+import Footer from "./components/Footer";
 
 
 const [searchTerm, setSearchTerm] = useState([]);
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<Home/>}></Route>
           <Route path="/movies" element={<Movies/>}></Route>
         </Routes>
+        <Footer/>
         </div>
     </Router>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="container">
+            <div className="row row__column">
+                <Link to="/" className="">
+                    <figure className='footer__logo'>
+                        <img src='/the-movie-database-logo.png' className='footer__logo--img' alt="" />
+                    </figure>
+                </Link>
+                <div className="footer__list">
+                    <Link to="/" className="footer__link">Home</Link>
+                    <Link to="/movies" className="footer__link">Find your movie</Link>
+                    <Link to="/" className="footer__link">Contact</Link>
+                </div>
+                <div className="footer__copyright">Copyright &copy; 2025 Movies Database</div>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer;
