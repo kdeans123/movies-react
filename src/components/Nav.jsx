8bd83defb273631660b6b4fa2b090163c03d3278
Kat <kat.m.deans@gmail.com>
2025-01-22 20:53:15 -0500

    Correction

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 
 
 const Nav = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+    const [openMenu, setOpenMenu] = useState(false);
 
     function openMenu() {
-        setMenuOpen(true);
+        setOpenMenu(true);
         document.body.classList =+ " menu--open";
     }
 
