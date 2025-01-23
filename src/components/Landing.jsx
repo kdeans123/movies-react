8ac65cb0c7bd802efd37c218c7ef3780a3cb3a38
Kat <kat.m.deans@gmail.com>
2025-01-23 17:16:43 -0500

    Landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,30 +1,61 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const Landing = () => {
-
   const [searchTerm, setSearchTerm] = useState('');
-
+  const [movies, setMovies] = useState([]);
+  async function getMovieInfo(searchTerm) {
+    try {
+      const data = await fetch(
+        `http://www.omdbapi.com/?apikey=YOUR-API-KEY&s=${searchTerm}`
+      );
+      const movieData = await data.json();
+      // Check if there's an error message from the API response
+      if (movieData.Error) {
+        console.error('Error fetching movie info:', movieData.Error);
+        // You can also set an error state to display to the user here
+        return;
+      }
+      const result = movieData.Search;
+      setMovies(result);
+    } catch (error) {
+      console.error('Error fetching movie info:', error);
+    }
+  }
   return (
     <section id="landing">
-    <header>
-      <div className="header__container">
-        <div className="header__description">
-          <h1>America's most awarded movies platform</h1>
-          <h2>Find the movie you love</h2>
-          <div className="input__wrapper">
-            <input
-              type="text"
-              value={searchTerm}
-              onChange={(event) => setSearchTerm(event.target.value)}
-              className="search__input"/>
-                <FontAwesomeIcon icon="magnifying-glass onClick"/>
+      <header>
+        <div className="header__container">
+          <div className="header__description">
+            <h1>America's most awarded movies platform</h1>
+            <h2>Find the movie you love</h2>
+            <div className="input__wrapper">
+              <input
+                type="text"
+                value={searchTerm}
+                onChange={(event) => setSearchTerm(event.target.value)}
+                className="search__input"
+              />
+              <button onClick={() => getMovieInfo(searchTerm)}>
+                Search
+              </button>
+            </div>
+          </div>
+          <div>
+            {movies.map((movie, index) => (
+              <div key={index}>
+                <div>
+                  <img src={movie?.Poster} alt="movie poster" />
+                  <h4>{movie.Title}</h4>
+                  <h5>{movie.Year}</h5>
+                  <h5>{movie.Type}</h5>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-      </div>
-    </header>
-  </section>
-  )
-}
-
-export default Landing;
+      </header>
+    </section>
+  );
+};
+export default Landing; 
