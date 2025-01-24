9df66ebb30eede25343f49d48d2d34fddf4a1e10
Kat <kat.m.deans@gmail.com>
2025-01-24 09:34:30 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Movies from "./pages/Movies";
 import Nav from "./components/Nav";
-import Landing from "./components/Landing";
 import Footer from "./components/Footer";
 
 
@@ -11,7 +10,6 @@ function App() {
     <Router>
       <div className="App">
         <Nav/>
-        <Landing/>
         <Routes>
           <Route path="/" exact element={<Home/>}></Route>
           <Route path="/movies" element={<Movies/>}></Route>
