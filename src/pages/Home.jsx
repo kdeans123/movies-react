d6a7a1b725c89d5163529ffed732ff943784e006
Kat <kat.m.deans@gmail.com>
2025-01-24 09:05:17 -0500

    Correcting the home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
 
 
   return (
-  )
+  "")
 }
 
 export default Home;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import { movies } from "../data";
+// import { movies } from "../data";
 
 
 
